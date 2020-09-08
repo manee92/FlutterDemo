3bdea8bf6afa786b8982f79fedc0747cba882031
Maneesha Erandi <kmerandi25@gmail.com>
2020-09-08 16:51:27 +0530

    Add pdf and localization files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutterexamples/screens/home.dart';
+import 'package:flutterexamples/screens/pdf_creation/pdf_creator_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: PDFCreator(),
     );
   }
 }
--- lib/screens/pdf_creation/pdf_creator_screen.dart
+++ lib/screens/pdf_creation/pdf_creator_screen.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutterexamples/screens/pdf_creation/report_pdf.dart';
 
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PDFCreator(),
+    );
+  }
+}
+
 class PDFCreator extends StatefulWidget {
   @override
   _PDFCreatorState createState() => _PDFCreatorState();
